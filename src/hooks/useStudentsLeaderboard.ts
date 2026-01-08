9af7f1f7cfc83c44e4bbb00dd0c032bb05765d9f
mohamedAbdalrazek <mohamedbadawy296@gmail.com>
2026-01-08 18:48:42 +0200

    modify the query to take dynamic school id

--- src/hooks/useStudentsLeaderboard.ts
+++ src/hooks/useStudentsLeaderboard.ts
@@ -2,9 +2,10 @@ import { gql, type TypedDocumentNode } from "@apollo/client";
 import type { GetLeaderboardDataQuery} from "../utils/types";
 import { useQuery } from "@apollo/client/react";
 
-const GET_LEADERBOARD_DATA: TypedDocumentNode<GetLeaderboardDataQuery> = gql`
-    query ExampleQuery {
-        studentsLeaderboard(schoolId:1) {
+type SchoolID = number
+const GET_LEADERBOARD_DATA: TypedDocumentNode<GetLeaderboardDataQuery, SchoolID> = gql`
+    query getLeaderboard ($schoolId:SchoolID!){
+        studentsLeaderboard(schoolId:$schoolId) {
             students {
                 name {
                     en
@@ -27,7 +28,11 @@ const GET_LEADERBOARD_DATA: TypedDocumentNode<GetLeaderboardDataQuery> = gql`
 }
 `
 export default function useStudentsLeaderborad() {
-    const { loading, error, data } = useQuery(GET_LEADERBOARD_DATA);
+    const { loading, error, data } = useQuery(GET_LEADERBOARD_DATA, {
+        variables:{
+            schoolId:1
+        }
+    });
 
     return { loading, error, students: data?.studentsLeaderboard.students, school: data?.studentsLeaderboard.school }
 }
